769cf7602a285f9de0b20ef5943b1e1e9670b7f8
Casey Calkins <c.angelcalkins@outlook.com>
2020-02-18 20:05:24 -0600

    Database is setup through google. Getting errors when trying to connect it through JS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ButtonAppBar from './components/NavBar';
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
 function App() {
+
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ButtonAppBar />
     </div>
   );
 }
 
+app.listen(port, () => {
+  console.log(`Server is on ${port}.`)
+})
 export default App;
